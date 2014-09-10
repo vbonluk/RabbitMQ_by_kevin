c0c29407473bac2056a49acf8c33a11ce85fa112
gameDeveloper <gamedeveloper@gameDeveloper.local>
2014-09-10 12:34:55 +0800

    Initial Commit

---
+++ testRabbitMQ/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testRabbitMQ
+//
+//  Created by gameDeveloper on 14-9-10.
+//  Copyright (c) 2014年 gameDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testRabbitMQ/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testRabbitMQ
+//
+//  Created by gameDeveloper on 14-9-10.
+//  Copyright (c) 2014年 gameDeveloper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
